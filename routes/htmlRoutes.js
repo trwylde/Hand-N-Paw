c843f75b11c47d0e292686c94889eca864c0fbb5
CodeYoga80s <CodeYoga80s@gmail.com>
2018-11-25 20:07:22 -0500

    pushing up mani's updates

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,27 @@
+// var db = require("../models");
+var path = require("path");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/survey", function(req, res) {
+    // res.send("surveyBreeds.html");
+    // res.sendFile(path.join(__dirname, "../public/surveyBreeds.html"));
+    // console.log("this is hell");
+    // res.send("hello");
+    res.render("surveyBreeds");
+  });
+
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.send("404 - Page Not Found");
+  });
+};
