6dd7a1634f78fc32c7be8958da253ebbc1d1132c
CodeYoga80s <CodeYoga80s@gmail.com>
2018-11-27 18:42:03 -0500

    pusing up recent changes to git hub

--- config/orm.js
+++ config/orm.js
@@ -1,32 +1,81 @@
 var connection = require("./connection.js");
 
-// Object Relational Mapper (ORM)
 
-// The ?? signs are for swapping out table or column names
-// The ? signs are for swapping out other values
-// These help avoid SQL injection
-// https://en.wikipedia.org/wiki/SQL_injection
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+
 var orm = {
-  // selectWhere: function(tableInput, colToSearch, valOfCol) {
-  //   var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-  //   connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-  //     if (err) throw err;
-  //     console.log(result);
-  //   });
-  // },
+  selectWhere: function(id, cb) {
+    var queryString = "SELECT * FROM breeds WHERE id = ?";
+    connection.query(queryString, [id], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      cb(result);
+    });
+  },
 
 
 // Mani's query begins here
 
 
-  selectBreed: function(size, outdoor_space, child_safe, sociable, multi_animal_safe) {
-    var queryString = "SELECT * FROM breeds WHERE size = ? AND outdoor_space = ? and child_safe = ? and sociable = ? and multi_animal_safe = ?";
-    connection.query(queryString, [size, outdoor_space, child_safe, sociable, multi_animal_safe], function(err, result) {
+  selectBreed: function(size, outdoor_space, child_safe, sociable, multi_animal_safe, health_issues, shed_factor, exercise_needs,cb) {
+    var queryString = "SELECT * FROM breeds WHERE size = ? AND outdoor_space = ? and child_safe = ? and sociable = ? and multi_animal_safe = ? and health_issues = ? and shed_factor = ? and exercise_needs = ?";
+    connection.query(queryString, [size, outdoor_space, child_safe, sociable, multi_animal_safe, health_issues, shed_factor, exercise_needs], function(err, result) {
       if (err) throw err;
-      console.log(result);
+      //console.log(result);
+      cb(result);
+    });
+  },
+
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
   }
-
 
 // Mani's query ends here
 
