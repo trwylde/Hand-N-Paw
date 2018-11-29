d520e539f232a5723f9799714824b11e66fe64bb
trwylde <trwylde@gmail.com>
2018-11-28 20:32:37 -0500

    Final style commit.

--- public/assets/js/pet-find.js
+++ public/assets/js/pet-find.js
@@ -5,14 +5,13 @@ $(document).ready(function() {
     event.preventDefault();
     $("#search-results").empty();
 
-    var breed = $("#breed").val().trim();
     var size = $("#size").val().trim();
     var age = $("#age").val().trim();
     var zip = $("#zipCode").val().trim();
     var gender = $("#gender").val().trim();
 
     $.ajax({
-      url: "https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?key=" + devkey + "&animal=dog&location=" + zip + "&breed=" + breed + "&sex=" + gender + "&size=" + size + "&age=" + age + "&format=json",
+      url: "https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?key=" + devkey + "&animal=dog&location=" + zip + "&sex=" + gender + "&size=" + size + "&age=" + age + "&format=json",
       method: "GET",
       success: function(data, status) {
         var dogs = data.petfinder.pets;
@@ -23,12 +22,12 @@ $(document).ready(function() {
           console.log(dogs.pet[i].name.$t);
 
           var dogInfo = 
-            `<div class="card border-info" style="max-width:25rem;">
+            `<div class="card pet-card border-info bg-secondary" style="max-width:25rem;">
               <img class="img-thumbnail" src="${dogs.pet[i].media.photos.photo[3].$t}" alt="Card image cap">
-              <div class="card-body text-info">
+              <div class="card-body text-white text-center">
                 <h4 class="dog-name" class="card-title">${dogs.pet[i].name.$t}</h5>
                 <h5 class="dog-age">${dogs.pet[i].age.$t}</h5>
-                <h5 class="dog-age">${dogs.pet[i].sex.$t}</h5>
+                <h5 class="dog-gender">${dogs.pet[i].sex.$t}</h5>
                 <h5 class="dog-breed">${dogs.pet[i].breeds.breed.$t}</h5>
               </div>`;
             $("#search-results").append(dogInfo);
@@ -36,9 +35,9 @@ $(document).ready(function() {
         },
       })
       
-  $("#breed").val("");
+  $("#gender").val("");
   $("#size").val("");
   $("#age").val("");
   $("#zipCode").val(""); 
   })
-}); //Closes Document Ready 
+}); 
